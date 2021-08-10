ba7117d11167b7590de139289cd7316818272d9f
dahua <2792543106@qq.com>
2021-08-10 16:20:27 +0800

    test(github action): test

--- github-action/pages/index.tsx
+++ github-action/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                   target="_blank"
                   rel="noopener noreferrer"
               >
-                  Powered by{' '}
+                  Powered by{' AHAHA '}
                   <span className={styles.logo}>
                       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
                   </span>
